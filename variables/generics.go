a40df4e88195bcedb67fe7289d90da3c0729682c
Mike Pineau <mikepineau@Mikes-MacBook-Air.local>
2026-06-04 11:34:40 -0400

    feat: add control flows and variables examples

---
+++ variables/generics.go
@@ -0,0 +1,10 @@
+package main
+
+func SlicesIndex[S ~[]E, E comparable](s S, v E) int {
+	for i := range s {
+		if v == s[i] {
+			return i
+		}
+	}
+	return -1
+}
---
+++ variables/slices.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	var s []string
+	fmt.Println("uninint: ", s, s == nil, len(s) == 0)
+
+	s = make([]string, 3)
+	fmt.Println("emp: ", s, "len: ", len(s), "cap: ", cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+
+	fmt.Println("set: ", s)
+	fmt.Println("get: ", s[2])
+
+	fmt.Println("len: ", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("Appended: ", s)
+
+	l := s[2:5]
+
+	fmt.Println("sl1: ", l)
+
+	l = s[:5]
+	fmt.Println("sl2: ", l)
+
+	l = s[2:]
+	fmt.Println("sl3: ", l)
+
+	t := []string{"g", "h", "i"}
+	if slices.Equal(s, t) {
+		fmt.Println("s and t are equal")
+	}
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		twoD[i] = make([]int, 4)
+		for j := 0; j < 4; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+
+}
