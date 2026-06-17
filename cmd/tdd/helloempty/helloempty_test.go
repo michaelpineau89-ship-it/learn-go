ee86be2244aee4a798b971b7969002ecd09f01de
Mike Pineau <mikepineau@Mikes-MacBook-Air.local>
2026-06-16 22:10:06 -0400

    feat: add multilingual greetings to helloempty

--- cmd/tdd/helloempty/helloempty_test.go
+++ cmd/tdd/helloempty/helloempty_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestHello(t *testing.T) {
 	t.Run("Saying Hello to People", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertRightMessage(t, got, want)
@@ -14,8 +14,29 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertRightMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertRightMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Mathieu", "French")
+		want := "Bonjour, Mathieu"
+
+		assertRightMessage(t, got, want)
+	})
+
+	t.Run("in Esperanto", func(t *testing.T) {
+		got := Hello("Marko", "Esperanto")
+		want := "Saluton, Marko"
 
 		assertRightMessage(t, got, want)
 	})
--- cmd/tdd/helloempty/main.go
+++ cmd/tdd/helloempty/main.go
@@ -1,16 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+const Spanish = "SPANISH"
+const French = "FRENCH"
+const Esperanto = "ESPERANTO"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const esperantoHelloPrefix = "Saluton, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
+
+	var prefix string
+
+	switch strings.ToUpper(language) {
+	case Spanish:
+		prefix = spanishHelloPrefix
+	case French:
+		prefix = frenchHelloPrefix
+	case Esperanto:
+		prefix = esperantoHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
 
 	if name == "" {
-		return englishHelloPrefix + "World"
+		return prefix + "World"
 	}
-	return englishHelloPrefix + name
+
+	return prefix + name
 }
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
